07fd978b45f915febb07cec24a00e4592d023146
Philipp Zedler <philipp@neue-musik.com>
2018-02-09 11:44:39 +0100

    Fix roundoff error for lat & lng on German site for OSM

--- djangocms_maps/static/djangocms_maps/js/mapbox.js
+++ djangocms_maps/static/djangocms_maps/js/mapbox.js
@@ -85,7 +85,10 @@ djangocms.Maps = {
         // latitute or longitute have precedence over the address when provided
         // inside the plugin form
         if (data.lat.length && data.lng.length) {
-            var latlng = { lat: parseFloat(data.lat), lng: parseFloat(data.lng) };
+            var latlng = {
+                lat: parseFloat(data.lat.replace(',', '.')),
+                lng: parseFloat(data.lng.replace(',', '.'))
+            };
             map.setView(latlng, data.zoom);
             this.addMarker(map, latlng, data);
         } else {
